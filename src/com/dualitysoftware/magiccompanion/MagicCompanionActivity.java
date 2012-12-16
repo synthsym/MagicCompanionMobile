a4b5977c040bcfd748be9c310abe1d45d7ce3cad
Proman21 <snoopyk921@gmail.com>
2012-12-16 22:48:44 +1000

    Modified UI to contain add, sub and reset buttons; created methods for each.

--- src/com/dualitysoftware/magiccompanion/MagicCompanionActivity.java
+++ src/com/dualitysoftware/magiccompanion/MagicCompanionActivity.java
@@ -3,18 +3,37 @@ package com.dualitysoftware.magiccompanion;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 public class MagicCompanionActivity extends Activity {
-
+	
+	int health = 20;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.magic_companion_activity);
+		
+		TextView health = (TextView) findViewById(R.id.health);
+		health.setText((CharSequence) health);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.magic_companion_activity, menu);
 		return true;
+	}
+	
+	public void addHealth(View view) {
+		health += 1;
+	}
+	
+	public void subHealth(View view) {
+		health -= 1;
+	}
+	
+	public void resetHealth(View view) {
+		health = 20;
 	}
 }
